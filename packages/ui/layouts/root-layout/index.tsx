1772ddbef7aaaa505478a7b97ebf620c3590d6f8
Pooria Mehregan <pooriamehregan@gmail.com>
2025-10-09 20:18:49 +0200

    fix: livshendelse styling (#98)

--- packages/ui/layouts/root-layout/index.tsx
+++ packages/ui/layouts/root-layout/index.tsx
@@ -1,21 +1,20 @@
-import {Divider} from '@digdir/designsystemet-react';
-import type {ReactNode} from 'react';
+import { Divider } from "@digdir/designsystemet-react";
+import { Header } from "../../components/header/index.tsx";
+import { Footer } from "../../components/footer/index.tsx";
+import { queryClient } from "@packages/data-access/react-query/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import type { ReactNode } from "react";
 
-import {Header} from '../../components/header/index.tsx';
-import styles from './style.module.css';
-import {Footer} from '../../components/footer/index.tsx';
-import {queryClient} from '@packages/data-access/react-query/query-client';
-import {QueryClientProvider} from '@tanstack/react-query';
+import "@digdir/designsystemet-css";
+import "@digdir/designsystemet-theme";
+import styles from "./style.module.css";
 
-import '@digdir/designsystemet-css';
-import '@digdir/designsystemet-theme';
-
-export function RootLayout({children}: {children: ReactNode}) {
+export function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="no">
       <head>
-        <meta charSet="UTF-8"/>
-        <link rel="icon" href="./favicon.ico" type="image/x-icon"/>
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="./favicon.ico" type="image/x-icon" />
         <link
           rel="stylesheet"
           href="https://altinncdn.no/fonts/inter/v4.1/inter.css"
