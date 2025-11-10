b3b81468434ce602d2fbb079c8e5f4630cb95485
Pooria Mehregan <pooriamehregan@gmail.com>
2025-11-10 14:36:27 +0100

    feat: add azure deployment for Strapi (#138)

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'strapi-provider-upload-azure-storage',
+      providerOptions: {
+        connectionString: env('STORAGE_CONNECTION_STRING'),
+        containerName: env('STORAGE_CONTAINER_NAME'),
+        // ... other options
+      },
+    },
+  },
+});
