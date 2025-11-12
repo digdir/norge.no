221994a1b69ede3756f94a0675b79d9ec1447f88
Pooria Mehregan <pooriamehregan@gmail.com>
2025-11-12 13:36:37 +0100

    fix: azure strapi deploy 6

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default ({ env })_ => ({
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-azure-storage',
+      providerOptions: {
+        accountName: env('STORAGE_ACCOUNT_NAME'),
+        containerName: env('BLOB_CONTAINER_NAME'),
+      },
+    },
+  },
+});
